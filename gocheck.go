a8ff87df65da4e0cb1c990d3a960f4334c299f38
Ferran Tomas <ferrandinand@gmail.com>
2017-12-01 14:59:29 +0100

    Added first tests

---
+++ gocheck.go
@@ -0,0 +1,48 @@
+package gocheck
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+var timeoutMilliseconds int = 5000
+
+func createConnection(url string) http.Client {
+	client := http.Client{
+		Timeout: time.Duration(time.Duration(timeoutMilliseconds) * time.Millisecond),
+	}
+	return client
+}
+
+//Check Status Code
+func statusCode(url string) (int, error) {
+
+	client := createConnection(url)
+	response, err := client.Get(url)
+
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+		return 0, err
+	}
+	return response.StatusCode, nil
+}
+
+//Check String response
+func urlBody(url string) (string, error) {
+
+	client := createConnection(url)
+	response, err := client.Get(url)
+
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+	return string(body), nil
+
+}
